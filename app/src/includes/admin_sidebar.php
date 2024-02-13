0644e83cb946b82505e716470c1708dd5007ba9a
Pratik Giri <prateekgiree7@gmail.com>
2024-02-13 14:48:10 +0545

    Logout functionality for admin

--- app/src/includes/admin_sidebar.php
+++ app/src/includes/admin_sidebar.php
@@ -40,7 +40,7 @@
             <i class="fas fa-search"></i>
             <span class="nav-item">Search</span>
           </a></li>
-        <li><a href="#" class="logout">
+        <li><a href="../pages/logout.php" class="logout">
             <i class="fas fa-sign-out-alt"></i>
             <span class="nav-item">Log out</span>
           </a></li>

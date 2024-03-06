4d6c2490bcfb50a5d6e619e6429c5d6b79272960
Pratik Giri <prateekgiree7@gmail.com>
2024-03-06 10:56:23 +0545

    Server side validation

--- app/src/process/teacher_add.php
+++ app/src/process/teacher_add.php
@@ -1,56 +1,121 @@
-<!-- ADD STUDENT DATA IN DATABASE -->
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+//check if the user is logged in or not
+if (empty($_SESSION['email'])) {
+    header('location:../../public/index.php');
+    exit();
+} else {
+    ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<body>
-    <?php
-    //check if form has been submitted
-    if (isset($_POST['fullname'])) {
-        $name = $_POST['fullname'];
-        $email = $_POST['email'];
-        $address = $_POST['address'];
-        $contact = $_POST['contact'];
-        $role = $_POST['role'];
-        $password = md5($_POST['password']);
-        $cpassword = md5($_POST['cpass']);
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
 
-        if ($password == $cpassword) {
+    <body>
+
+        <?php
+        // Check if form has been submitted
+        if (isset($_POST['fullname'])) {
             include_once "../includes/connection.php";
+            $stmt = null;
+            $errors = array();
 
-            $sql = "INSERT INTO user(name,email,address,contact,role,password)VALUES(?,?,?,?,?,?)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ssssis", $name, $email, $address, $contact, $role, $password);
-            if ($stmt->execute()) {
-                echo " <script>
-                            alert('Registered successfully'); 
-                            window.location.href='../admin/admin_dashboard.php'; 
-                        </script>";
-            } else {
-                echo " <script>
-                            alert('Something went wrong'); 
-                            window.location.href='../admin/admin_dashboard.php'; 
-                        </script>";
-
+            $fields = array(
+                "fullname" => "Full Name",
+                "email" => "Email",
+                "address" => "Address",
+                "contact" => "Contact number",
+                "role" => "Role",
+                "password" => "Password",
+                "cpass" => "Confirm Password"
+            );
+            foreach ($fields as $field => $label) {
+                if ($_POST[$field] == '') {
+                    $errors[$field] = "{$label} is required";
+                } elseif (preg_match("/^\s*$/", $_POST[$field])) {
+                    $errors[$field] = "{$field} is invalid ";
+                } elseif ($field == "fullname" && !preg_match("/^[a-zA-Z ]{4,}$/", $_POST[$field])) {
+                    $errors[$field] = "Name should only contain alphabet and be at least 4 characters long";
+                } elseif ($field == "email" && !filter_var($_POST[$field], FILTER_VALIDATE_EMAIL)) {
+                    $errors[$field] = "{$_POST[$field]} is invalid email format";
+                } elseif ($field == "contact" && !preg_match("/^(98|97)\d{8}$/", $_POST[$field])) {
+                    $errors[$field] = "{$_POST[$field]} is invalid contact number";
+                } elseif ($field == "password") {
+                    if (strlen($_POST[$field]) < 8) {
+                        $errors[$field] = "{$label} should be at least 8 characters long";
+                    }
+                    if (strtolower($_POST[$field]) === strtolower("Password")) {
+                        $errors[$field] = "{$label} cannot not be password";
+                    }
+                    if (!preg_match('/^(?=.*[A-Za-z])(?=.*[^A-Za-z0-9]).{8,25}$/', $_POST[$field])) {
+                        $errors[$field] = "{$label} should contain alphabet and at least one special character";
+                    }
+                }
             }
-            $stmt->close();
-            $conn->close();
-        } else {
-            echo " <script>
-                        alert('Password and confirm password did not match');
-                        window.location.href='../pages/teacher_add.php'
+
+            if (!empty($errors)) {
+                foreach ($errors as $error) {
+                    // echo "<p>{$error}<p>";
+                    echo "<script>alert('{$error}');
+                    window.location.href = '../pages/teacher_add.php';
                     </script>";
 
-        }
-    } else {
-        header("Location: ../../public/index.php");
-        exit();
-    }
-    ?>
-</body>
+                }
+            } else {
+                $name = trim($_POST['fullname']);
+                $email = $_POST['email'];
+                $address = $_POST['address'];
+                $contact = $_POST['contact'];
+                $role = intval($_POST['role']);
+                $password = md5(trim($_POST['password']));
+                $cpassword = md5(trim($_POST['cpass']));
 
-</html>
+                if ($password != $cpassword) {
+                    echo "<script>alert('Password and confirm password did not match');
+                    window.location.href = '../pages/teacher_add.php';
+                    </script>";
+                } else {
+                    try {
+                        // Prepare and execute SQL statement
+                        $sql = "INSERT INTO user (name, email, address, contact, role, password) VALUES (?, ?, ?, ?, ?, ?)";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->bind_param("ssssis", $name, $email, $address, $contact, $role, $password);
+
+                        if ($stmt->execute()) {
+                            echo "<script>alert('Registered successfully');
+                            window.location.href ='../admin/admin_dashboard.php';</script>";
+                            exit();
+                        } else {
+                            echo "<script>alert('Something went wrong');
+                            window.location.href = '../admin/admin_dashboard.php';</script>";
+                            exit();
+                        }
+                    } catch (Exception $e) {
+                        ?>
+                        <script>alert("Teacher with this contact info already exist");
+                            window.location.href = '../pages/teacher_add.php';
+                        </script>
+                        <?php
+                    }
+                }
+            }
+            // Close statement and connection
+            if ($stmt !== null) {
+                $stmt->close();
+            }
+            $conn->close();
+        } else {
+            header("Location:../admin/admin_dashboard.php");
+            exit();
+        }
+        ?>
+    </body>
+
+    </html>
+    <?php
+}
+?>
